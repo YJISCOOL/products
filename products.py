94b43941522278f95fa8b09f6f7d853f7dbfd3a9
YJISCOOL <star000321@gmail.com>
2020-10-24 11:05:09 +0800

    add header

--- products.py
+++ products.py
@@ -3,7 +3,7 @@ while True:
 	name = input('input product: ')
 	if name == 'q':
 		break
-	price = input('inpit price: ') 
+	price = int(input('inpit price: ')) #所以現在price都是整數的形式
 	#有輸入商品時再輸入價格
 
 	shop_cart.append([name, price])
@@ -14,4 +14,11 @@ while True:
 print (shop_cart)
 
 for product in shop_cart:
-	print (product[0], '的價格是', product[1])
+	print (product[0], '價格是', str(product[1]))
+
+with open('products.csv', 'w', encoding = 'utf-8') as f1: 
+#只是打開檔案
+	f1.writ('商品,價格\n')
+	for product in shop_cart:
+		f1.write(product[0] + ',' + str(product[1]) + '\n') #記得轉換整數與字串的形式
+		#才真正有寫入檔案
