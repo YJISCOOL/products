26782247ff0f75fb67e26eb42074c7b469aa9370
YJISCOOL <star000321@gmail.com>
2020-10-24 09:22:35 +0800

    products

--- products.py
+++ products.py
@@ -5,11 +5,9 @@ while True:
 		break
 	price = input('inpit price: ') 
 	#有輸入商品時再輸入價格
-	pro_pri =[]
-	pro_pri.append(name)
-	pro_pri.append(price)
+	pro_pri =[name, price]
+	#上列等於pro_pri = []
+	#pro_pri.append(name)
+	#pro_pri.append(price)
 	shop_cart.append(pro_pri)
 print (shop_cart)
-
-print (shop_cart[0][0])
-print (shop_cart[0][1])
